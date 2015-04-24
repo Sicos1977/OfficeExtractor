169b364c0aaf1806285799a70e534889a728e38d
Kees <sicos2002@hotmail.com>
2015-04-24 07:57:48 +0200

    Added PowerPoint class

---
+++ OfficeExtractor/PowerPoint.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using CompoundFileStorage;
+using OfficeExtractor.Exceptions;
+using OfficeExtractor.Helpers;
+
+namespace OfficeExtractor
+{
+    /// <summary>
+    /// This class is used as a placeholder for all PowerPoint related methods
+    /// </summary>
+    internal static class PowerPoint
+    {
+        #region SaveToFolder
+        /// <summary>
+        /// This method saves all the PowerPoint embedded binary objects from the <paramref name="inputFile"/> to the
+        /// <paramref name="outputFolder"/>
+        /// </summary>
+        /// <param name="inputFile">The binary PowerPoint file</param>
+        /// <param name="outputFolder">The output folder</param>
+        /// <returns></returns>
+        /// <exception cref="OEFileIsPasswordProtected">Raised when the <see cref="inputFile"/> is password protected</exception>
+        public static List<string> SaveToFolder(string inputFile, string outputFolder)
+        {
+            using (var compoundFile = new CompoundFile(inputFile))
+            {
+                if (IsPasswordProtected(compoundFile))
+                    throw new OEFileIsPasswordProtected("The file '" + Path.GetFileName(inputFile) +
+                                                        "' is password protected");
+
+                var result = new List<string>();
+
+                if (!compoundFile.RootStorage.ExistsStream("PowerPoint Document")) return result;
+                var stream = compoundFile.RootStorage.GetStream("PowerPoint Document") as CFStream;
+                if (stream == null) return result;
+
+                using (var memoryStream = new MemoryStream(stream.GetData()))
+                using (var binaryReader = new BinaryReader(memoryStream))
+                {
+                    while (binaryReader.BaseStream.Position != memoryStream.Length)
+                    {
+                        var verAndInstance = binaryReader.ReadUInt16();
+                        // ReSharper disable once UnusedVariable
+                        var version = verAndInstance & 0x000FU; // first 4 bit of field verAndInstance
+                        var instance = (verAndInstance & 0xFFF0U) >> 4; // last 12 bit of field verAndInstance
+
+                        var typeCode = binaryReader.ReadUInt16();
+                        var size = binaryReader.ReadUInt32();
+                        //var isContainer = (version == 0xF);
+
+                        // Embedded OLE objects start with code 4113
+                        if (typeCode == 4113)
+                        {
+                            if (instance == 0)
+                            {
+                                // Uncompressed
+                                var bytes = binaryReader.ReadBytes((int)size);
+
+                                // Check if the ole object is another compound storage node with a package stream
+                                if (bytes[0] == 0xD0 && bytes[1] == 0xCF)
+                                    bytes = Extraction.GetBytesFromCompoundPackageStream(bytes);
+
+                                result.Add(Extraction.SaveByteArrayToFile(bytes, outputFolder + "Embedded object"));
+                            }
+                            else
+                            {
+                                var decompressedSize = binaryReader.ReadUInt32();
+                                var data = binaryReader.ReadBytes((int)size - 4);
+                                var compressedMemoryStream = new MemoryStream(data);
+
+                                // skip the first 2 bytes
+                                compressedMemoryStream.ReadByte();
+                                compressedMemoryStream.ReadByte();
+
+                                // Decompress the bytes
+                                var decompressedBytes = new byte[decompressedSize];
+                                var deflateStream = new DeflateStream(compressedMemoryStream, CompressionMode.Decompress,
+                                    true);
+                                deflateStream.Read(decompressedBytes, 0, decompressedBytes.Length);
+
+                                // Check if the ole object is another compound storage node with a package stream
+                                if (decompressedBytes[0] == 0xD0 && decompressedBytes[1] == 0xCF)
+                                    decompressedBytes = Extraction.GetBytesFromCompoundPackageStream(decompressedBytes);
+
+                                result.Add(Extraction.SaveByteArrayToFile(decompressedBytes, outputFolder + "Embedded object"));
+                            }
+                        }
+                        else
+                            binaryReader.BaseStream.Position += size;
+                    }
+                }
+
+                return result;
+            }
+        }
+        #endregion
+
+        #region IsPasswordProtected
+        /// <summary>
+        /// Returns true when the binary PowerPoint file is password protected
+        /// </summary>
+        /// <param name="compoundFile"></param>
+        /// <returns></returns>
+        private static bool IsPasswordProtected(CompoundFile compoundFile)
+        {
+            if (!compoundFile.RootStorage.ExistsStream("Current User")) return false;
+            var stream = compoundFile.RootStorage.GetStream("Current User") as CFStream;
+            if (stream == null) return false;
+
+            using (var memoryStream = new MemoryStream(stream.GetData()))
+            using (var binaryReader = new BinaryReader(memoryStream))
+            {
+                var verAndInstance = binaryReader.ReadUInt16();
+                // ReSharper disable UnusedVariable
+                // We need to read these fields to get to the correct location in the Current User stream
+                var version = verAndInstance & 0x000FU;         // first 4 bit of field verAndInstance
+                var instance = (verAndInstance & 0xFFF0U) >> 4; // last 12 bit of field verAndInstance
+                var typeCode = binaryReader.ReadUInt16();
+                var size = binaryReader.ReadUInt32();
+                var size1 = binaryReader.ReadUInt32();
+                // ReSharper restore UnusedVariable
+                var headerToken = binaryReader.ReadUInt32();
+
+                switch (headerToken)
+                {
+                    // Not encrypted
+                    case 0xE391C05F:
+                        return false;
+
+                    // Encrypted
+                    case 0xF3D1C4DF:
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        }
+        #endregion
+    }
+}
