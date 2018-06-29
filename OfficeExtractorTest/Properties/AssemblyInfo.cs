35326558834e079b3ba049ecce3127df9434706a
sicos1977 <sicos2002@hotmail.com>
2018-06-29 18:28:03 +0200

    Upped version number

--- OfficeExtractorTest/Properties/AssemblyInfo.cs
+++ OfficeExtractorTest/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OfficeExtractorTest")]
-[assembly: AssemblyCopyright("Copyright © 2013 - 2017 Kees van Spelde")]
+[assembly: AssemblyCopyright("Copyright © 2013 - 2018 Kees van Spelde")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyFileVersion("1.6.2.0")]
